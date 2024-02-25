1289beeb6237329b30df4295203fd0f494aba7e7
Amila Jahic <amila.jahic@infobip.com>
2024-02-25 21:16:53 +0100

    Second version

--- src/main/java/com/example/weather_station/Weather.java
+++ src/main/java/com/example/weather_station/Weather.java
@@ -2,8 +2,10 @@ package com.example.weather_station;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class Weather {
+public class Weather implements Serializable {
     private Location location;
     private Current current;
 }
---
+++ src/main/java/com/example/weather_station/WeatherInfo.java
@@ -0,0 +1,14 @@
+//package com.example.weather_station;
+//
+//import lombok.Data;
+//
+//@Data
+//public class WeatherInfo {
+//    private String cityName;
+//    private String weatherCondition;
+//
+//    public WeatherInfo(String cityName, String weatherCondition){
+//        this.cityName = cityName;
+//        this.weatherCondition = weatherCondition;
+//    }
+//}
