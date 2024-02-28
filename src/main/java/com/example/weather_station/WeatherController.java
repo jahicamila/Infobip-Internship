368737f322fd4158dd975f4123a4bc05fdfe2dd8
Amila Jahic <amila.jahic@infobip.com>
2024-02-28 13:46:44 +0100

    Version: Storing data in List

--- src/main/java/com/example/weather_station/WeatherController.java
+++ src/main/java/com/example/weather_station/WeatherController.java
@@ -24,12 +24,20 @@ public class WeatherController {
 
     private static final String filepath = "weather_info.ser";
 
+    private final List<WeatherInfo> weatherInfoList = new ArrayList<>();
+
     @GetMapping
     public List<WeatherInfo> getWeatherData() {
-        return readWeatherData();
+        List<WeatherInfo> allWeatherData = readWeatherData();
+        if (allWeatherData.size() > 2) {
+            return allWeatherData.subList(allWeatherData.size() - 2, allWeatherData.size());
+        } else {
+            return allWeatherData;
+        }
     }
-    @Scheduled(fixedRate = 3600000)
-    public void updateWeatherData(){
+
+    @Scheduled(fixedRate = 20000)
+    public void updateWeatherData() {
         try {
             WeatherInfo weather = fetchWeatherData();
             writeWeatherData(weather);
@@ -39,30 +47,31 @@ public class WeatherController {
     }
 
     private WeatherInfo fetchWeatherData() throws IOException, InterruptedException {
-            HttpClient client = HttpClient.newHttpClient();
+        HttpClient client = HttpClient.newHttpClient();
 
-            HttpRequest request = HttpRequest
-                    .newBuilder()
-                    .uri(URI.create("http://api.weatherapi.com/v1/current.json?key=79dcb7239b0b4d7daa8112246242202&q=Sarajevo"))
-                    .build();
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .uri(URI.create("http://api.weatherapi.com/v1/current.json?key=79dcb7239b0b4d7daa8112246242202&q=Sarajevo"))
+                .build();
 
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            Weather weather = objectMapper.readValue(response.body(), Weather.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Weather weather = objectMapper.readValue(response.body(), Weather.class);
 
-            String cityName = weather.getLocation().getName();
-            String weatherCondition = weather.getCurrent().getCondition().getText();
-            LocalDateTime currentTime = LocalDateTime.now();
+        String cityName = weather.getLocation().getName();
+        String weatherCondition = weather.getCurrent().getCondition().getText();
+        LocalDateTime currentTime = LocalDateTime.now();
 
-            return new WeatherInfo(cityName, weatherCondition, currentTime);
-        }
+        return new WeatherInfo(cityName, weatherCondition, currentTime);
+    }
 
 
     private void writeWeatherData(WeatherInfo weatherInfo) {
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filepath, true))) {
-            outputStream.writeObject(weatherInfo);
+        weatherInfoList.add(weatherInfo);
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filepath))) {
+            outputStream.writeObject(weatherInfoList);
             System.out.println("Weather information has been written to the file: " + filepath);
         } catch (IOException e) {
             e.printStackTrace();
@@ -72,17 +81,7 @@ public class WeatherController {
     private List<WeatherInfo> readWeatherData() {
         List<WeatherInfo> weathers = new ArrayList<>();
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filepath))) {
-            LocalDateTime twoHoursAgo = LocalDateTime.now().minusHours(2);
-            while (true) {
-                try {
-                    WeatherInfo weatherInfo = (WeatherInfo) inputStream.readObject();
-                    if (weatherInfo.getLocalTime().isAfter(twoHoursAgo)) {
-                        weathers.add(weatherInfo);
-                    }
-                } catch (EOFException e) {
-                    break;
-                }
-            }
+                weathers = (List<WeatherInfo>) inputStream.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
